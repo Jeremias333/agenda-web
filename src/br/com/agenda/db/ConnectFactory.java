7e40c01fa214e3075b87d2e18b9c11b3df9eebaf
Jeremias S <jeremiasprogrammer@gmail.com>
2020-05-09 21:35:09 -0300

    Modificado para troca de exception.

--- src/br/com/agenda/db/ConnectFactory.java
+++ src/br/com/agenda/db/ConnectFactory.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.servlet.ServletException;
+
 public class ConnectFactory {
 	
 	public String db = "web";
@@ -14,12 +16,12 @@ public class ConnectFactory {
 	
 	public String url = "jdbc:mysql://"+ip+":"+port+"/"+db+"?useTimezone=true&serverTimezone=UTC";
 	
-	public Connection getConnection() {
+	public Connection getConnection() throws ServletException {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			return DriverManager.getConnection(url, user, password);
 		} catch (SQLException | ClassNotFoundException e) {
-			throw new RuntimeException(e);
+			throw new ServletException(e);
 		}
 	}		
 }
